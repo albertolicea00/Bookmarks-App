9ee4e9a27be09b962519b1410bcf836777d5997c
Alberto Licea <albertolicea00@icloud.com>
2023-10-29 15:46:40 -0400

    add initial automatization

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Activar el entorno virtual de tu proyecto Django
+source env/Scripts/bin/activate
+
+# Ejecutar el back-end
+cd api
+python manage.py migrate
+python manage.py runserver
+cd ..
+
+# Ejecutar el front-end
+cd app
+npm run serve
+cd..
